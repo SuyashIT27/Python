1f03f03cfeb9b951dd719bd49567b3e44d73ccbd
Suyash Verma <suyashvermaabes27@gmail.com>
2026-03-19 09:53:30 +0530

    Add files via upload

---
+++ Lectures/Lecture1.py
@@ -0,0 +1,18 @@
+print("Hello World!")
+# we do not need to provide integer type for variable
+name="Suyash Verma"
+id=257
+percentage=67.5
+print(id)
+#type() can be used to know the variable type 
+print(type(name))
+#Python is case senstitive 
+# There are total 35  keywords in the python 
+
+#Program to print  the sum of two numbers 
+a=int(input("Enter the number 1"))
+b=int(input("Enter the number 2"))
+print("The sun is ",a+b)
+#Type Conversion is the technique of convertig on datatype into rhe another 
+#it is of two type type conversion interpreter done it by own and type casting by the user 
+
